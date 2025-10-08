090b2e1cadffab858238bed14a87fd3b8769b32d
anubhavsingh2027 <anubhavsingh2027@gmail.com>
2025-10-08 18:39:44 +0530

    change error of post login

--- controllers/Auth-Controller.js
+++ controllers/Auth-Controller.js
@@ -59,7 +59,7 @@ exports.postLogin = async (req, res) => {
     });
   } catch (err) {
     console.error("Login error:", err);
-    res.status(500).json({ status: false, message: "Server error during login" });
+    res.status(500).json({ status: false, message: `${err}` });
   }
 };
 // ===== LOGOUT =====
