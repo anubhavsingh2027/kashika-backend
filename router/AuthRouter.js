ac7e748c5571a43ff275c26b0e604194bd59d614
anubhavsingh2027 <anubhavsingh2027@gmail.com>
2025-10-09 16:24:11 +0530

    update all roter

--- router/AuthRouter.js
+++ router/AuthRouter.js
@@ -8,17 +8,5 @@ AuthRouter.post("/login", AuthController.postLogin);
 AuthRouter.post("/logout", AuthController.postLogout);
 AuthRouter.post("/forgetPassword",AuthController.postForget);
 
-// Example of protected route using JWT
-AuthRouter.get(
-  "/profile",
-  AuthController.verifyJWT,
-  (req, res) => {
-    res.status(200).json({
-      status: true,
-      message: "Protected route accessed successfully",
-      user: req.user,
-    });
-  }
-);
 
 module.exports = AuthRouter;
